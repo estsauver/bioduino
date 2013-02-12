73246269cbd5f5c1886cf40a4e2ec1be4f5a424b
Earl St Sauver <estsauver@gmail.com>
2013-02-12 18:23:20 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bioduino::Application.config.secret_token = '644525cfa69546e393fbb6541548833b321ffe0f4a970251cc9307d8206164d57639cacf6ef733381da6a6be9dd58ef8502e345ae7c4a9d5f607fc789595f104'
