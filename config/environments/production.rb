e54a852c9fbb69faa321b709eafe9614f8a632ad
Earl St Sauver <estsauver@gmail.com>
2013-02-13 12:36:57 -0500

    add precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,8 @@ Bioduino::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
+
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -21,7 +22,7 @@ Bioduino::Application.configure do
   config.assets.digest = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
-  # config.assets.manifest = YOUR_PATH
+  config.assets.manifest = Rails.root.join("public/assets")
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
@@ -46,7 +47,7 @@ Bioduino::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+   config.assets.precompile += %w( experiments.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
